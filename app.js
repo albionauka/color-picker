e2035f3d22fdc657ca2bb74e9c9cd1c7fd305e22
albionauka <albionauka@gmail.com>
2022-02-01 18:35:27 -0800

    Finished the color flipper

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+const colors = ["green", "red", "slategrey", "rgb(52, 125, 235)"]
+const color = document.querySelector('.color');
+const btn = document.getElementById('btn');
+btn.addEventListener("click", () => {
+    let randomNumber = getRandomNumber()
+    document.body.style.background = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+})
 
+function getRandomNumber () {
+    return Math.floor(Math.random()* colors.length);
+}
--- hex.js
+++ hex.js
@@ -1,1 +1,15 @@
+const colors = [1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D","E","F"]
+const color = document.querySelector('.color');
+const btn = document.getElementById('btn');
 
+btn.addEventListener('click', () =>{
+    let hexColor = "#";
+    for(let i = 0; 1 < 6; i += 1) {
+        hexColor += colors[getRandomNumber()];
+    }
+    console.log(hexColor)
+})
+
+function getRandomNumber () {
+    return Math.floor(Math.random() * colors.length);
+}
